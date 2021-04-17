54d9f41a6d7e01145e33f8533ffae38374aa2f45
Astro <sm@m-labs.hk>
2021-04-18 00:55:01 +0200

    autounattend: use ForceShutdownNow instead of FirstLogonCommands shutdown
    this moves the return a little earlier in the installation process.
    it is less of a hack, and less problematic with custom `defaultUser`
    settings.

--- wfvm/autounattend.nix
+++ wfvm/autounattend.nix
@@ -275,14 +275,12 @@ let
     </AutoLogon>
   ''}
 
-          <FirstLogonCommands>
-            <SynchronousCommand wcm:action="add">
-              <Order>1</Order>
-              <CommandLine>cmd /C shutdown /s /f /t 00</CommandLine>
-              <Description>ChangeHideFiles</Description>
-            </SynchronousCommand>
-          </FirstLogonCommands>
-
+        </component>
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+          <Reseal>
+            <ForceShutdownNow>true</ForceShutdownNow>
+            <Mode>OOBE</Mode>
+          </Reseal>
         </component>
       </settings>
 
